bcdbabb6e32c1ead0b70f08c182d9806f49abefe
Truman Kilen <t@kilen.me>
2025-01-17 20:30:13 -0600

    Add compress function

--- src/compression.rs
+++ src/compression.rs
@@ -1,11 +1,30 @@
 use anyhow::{bail, Result};
-use std::io::Read as _;
+use std::io::{Read as _, Write};
 use strum::{AsRefStr, EnumString};
 
 #[derive(Debug, Clone, Copy, EnumString, AsRefStr)]
 pub enum CompressionMethod {
     Zlib,
     Oodle,
+}
+
+pub fn compress<S: Write>(compression: CompressionMethod, input: &[u8], output: S) -> Result<()> {
+    match compression {
+        CompressionMethod::Zlib => {
+            let mut encoder = flate2::write::ZlibEncoder::new(output, flate2::Compression::best());
+            encoder.write_all(input)?;
+            encoder.finish()?;
+        }
+        CompressionMethod::Oodle => {
+            oodle_loader::oodle()?.compress(
+                input,
+                output,
+                oodle_loader::Compressor::Mermaid,
+                oodle_loader::CompressionLevel::Normal,
+            )?;
+        }
+    }
+    Ok(())
 }
 
 pub fn decompress(compression: CompressionMethod, input: &[u8], output: &mut [u8]) -> Result<()> {
